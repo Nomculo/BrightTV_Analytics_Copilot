fd4c825afbb1dde2575b3c1212db904efdfa109a
Nomculo <khumalonomculo@gmail.com>
2026-07-19 17:47:13 +0000

    push 2

---
+++ Insights/Findings.sql
@@ -0,0 +1,9 @@
+-- Databricks notebook source
+ Insight
+
+Q1: The dataset contains 5,375 subscribers. This figure represents the current subscriber base and serves as a baseline for future growth analysis.
+
+Q2: Gauteng has the largest subscriber base, while Limpopo has significantly fewer subscribers. BrightTV could investigate whether lower subscriber numbers are due to population size, limited marketing, or lower service adoption, and consider targeted campaigns in underrepresented provinces.
+
+Q3: The dataset contains four distinct gender values. Two of these values do not clearly identify the subscriber's gender, indicating incomplete or inconsistent demographic data.
+The presence of unspecified gender values may reduce the accuracy of audience segmentation and personalized marketing. Improving the quality of this demographic data could help BrightTV better understand its subscriber base, support more targeted marketing campaigns, and make more informed content recommendations where demographic segmentation is appropriate.
