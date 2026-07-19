2451de8f5a30d788b2596d3868ef31bc1ad80871
Nomculo <khumalonomculo@gmail.com>
2026-07-19 19:49:11 +0200

    Delete Insights/New Notebook 2026-07-19 19_26_56.sql

--- Insights/New Notebook 2026-07-19 19_26_56.sql
+++
@@ -1,5 +0,0 @@
--- Databricks notebook source
-Q:1 Insight
-
-The dataset contains 5,375 subscribers. This figure represents the current subscriber base and serves as a baseline for future growth analysis.
-
